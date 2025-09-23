bf6d31dfb7f03b1bdeb59da1ac52d1d30d8ec4a6
TommyHallowed <studenttomas2126@gmail.com>
2025-09-23 23:06:55 +0200

    Fixed the texture of Rainbow wool and carpet

--- main/java/net/hallowed/TheOldWays.java
+++ main/java/net/hallowed/TheOldWays.java
@@ -1,42 +1,60 @@
 package net.hallowed;
 
 import net.fabricmc.api.ModInitializer;
-
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.hallowed.oldways.content.ModBlocks;
 import net.hallowed.oldways.content.ModItems;
 import net.hallowed.oldways.config.CommonConfigManager;
 import net.hallowed.oldways.mending.MendingNerf;
+import net.hallowed.oldways.mixin.BlockEntityTypeBlocksAccessor;   // <-- our accessor
 import net.hallowed.oldways.network.NetworkInit;
 import net.hallowed.oldways.network.ServerHandlers;
+import net.minecraft.block.Block;
+import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.item.ItemGroups;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 public class TheOldWays implements ModInitializer {
-	public static final String MOD_ID = "old-ways";
+    public static final String MOD_ID = "old-ways";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
-	@Override
-	public void onInitialize() {
+    @Override
+    public void onInitialize() {
+        // Register blocks then items (must be in this order)
         ModBlocks.register();
         ModItems.register();
+
+        // Add our custom blocks to vanilla BlockEntityType allow-lists
+        addSupported(BlockEntityType.BED, ModBlocks.RAINBOW_BED);
+        addSupported(BlockEntityType.BANNER, ModBlocks.RAINBOW_BANNER, ModBlocks.RAINBOW_WALL_BANNER);
+
+        // Rest of your init
         CommonConfigManager.load();
         MendingNerf.init();
         NetworkInit.register();
         ServerHandlers.register();
 
-        // Add to creative tabs *after* registration (if you use Fabric API)
-         ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
-             entries.add(ModItems.RAINBOW_WOOL);
-             entries.add(ModItems.RAINBOW_CARPET);
-             entries.add(ModItems.RAINBOW_BED);
-             entries.add(ModItems.RAINBOW_BANNER);
-         });
-		LOGGER.info("The Old Ways Mod Loaded!");
-	}
+        // Put items on creative tabs AFTER registration
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
+            entries.add(ModItems.RAINBOW_WOOL);
+            entries.add(ModItems.RAINBOW_CARPET);
+            entries.add(ModItems.RAINBOW_BED);
+            entries.add(ModItems.RAINBOW_BANNER);
+        });
+
+        LOGGER.info("The Old Ways Mod Loaded!");
+    }
+
+    /** Adds blocks to the private supported-blocks set on a BlockEntityType (bed/banner need this). */
+    private static void addSupported(BlockEntityType<?> type, Block... toAdd) {
+        BlockEntityTypeBlocksAccessor acc = (BlockEntityTypeBlocksAccessor) type;
+        Set<Block> copy = new HashSet<>(acc.oldways$getBlocks()); // make mutable copy
+        Collections.addAll(copy, toAdd);
+        acc.oldways$setBlocks(copy); // write back
+    }
 }
