0772befe9e000b9bc6bbf3df22cf70bce611413e
TommyHallowed <studenttomas2126@gmail.com>
2025-10-01 22:12:59 +0200

    Moved support block entity line to main class

--- src/main/java/net/hallowed/TheOldWays.java
+++ src/main/java/net/hallowed/TheOldWays.java
@@ -1,14 +1,12 @@
 package net.hallowed;
 
 import net.fabricmc.api.ModInitializer;
-import net.hallowed.oldways.api.OWBlockEntityTypeSupport;
 import net.hallowed.oldways.api.OWRegistry;
 import net.hallowed.oldways.config.CommonConfigManager;
 import net.hallowed.oldways.content.*;
 import net.hallowed.oldways.content.feature.BoneMealExpansion;
 import net.hallowed.oldways.enchantment.MendingNerf;
 import net.hallowed.oldways.network.OldWaysNetwork;
-import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.item.Items;
 import org.slf4j.Logger;
@@ -31,9 +29,6 @@ public class TheOldWays implements ModInitializer {
         ModDataComponents.init();
         MendingNerf.init();
         BoneMealExpansion.init();
-
-        // 2) Extend Bed block-entity support for custom bed
-        OWBlockEntityTypeSupport.addSupported(BlockEntityType.BED, ModBlocks.RAINBOW_BED);
 
         // 3) Creative tab entries
         OWRegistry.addToGroup(ItemGroups.COLORED_BLOCKS, entries -> {
