5ab8b394a97726241c585fd523e7d26da36e9dab
TommyHallowed <studenttomas2126@gmail.com>
2025-10-03 07:08:15 +0200

    Added config option to disable copy screenshot mechanic

--- src/client/java/net/hallowed/oldways/client/clipboard/CopyScreenshotHelper.java
+++ src/client/java/net/hallowed/oldways/client/clipboard/CopyScreenshotHelper.java
@@ -1,5 +1,6 @@
 package net.hallowed.oldways.client.clipboard;
 
+import net.hallowed.oldways.client.config.ClientConfigManager;
 import org.jetbrains.annotations.NotNull;
 
 import javax.imageio.ImageIO;
@@ -14,6 +15,7 @@ public final class CopyScreenshotHelper {
 
     /** Copy given file (PNG) to system clipboard (AWT). */
     public static void copyFromFile(File file) {
+        if (!ClientConfigManager.copyScreenshotsToClipboard()) return;
         try {
             BufferedImage img = ImageIO.read(file);
             if (img == null) return;
--- src/client/java/net/hallowed/oldways/client/mixin/screenshot/MainMixin.java
+++ src/client/java/net/hallowed/oldways/client/mixin/screenshot/MainMixin.java
@@ -1,5 +1,6 @@
 package net.hallowed.oldways.client.mixin.screenshot;
 
+import net.hallowed.oldways.client.config.ClientConfigManager;
 import net.minecraft.client.main.Main;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,6 +13,7 @@ import java.util.Locale;
 public class MainMixin {
     @Inject(method = "main", at = @At("HEAD"), remap = false)
     private static void oldways$enableAwt(CallbackInfo ci) {
+        if (!ClientConfigManager.copyScreenshotsToClipboard()) return;
         if (!System.getProperty("os.name").toLowerCase(Locale.ROOT).contains("mac")) {
             System.setProperty("java.awt.headless", "false");
         }
