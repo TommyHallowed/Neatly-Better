63f1fae7b570dc1924b05f022484a195cb48d2de
TommyHallowed <studenttomas2126@gmail.com>
2025-10-08 03:45:12 +0200

    Vexes now die when their evoker is killed

---
+++ src/main/java/net/hallowed/oldways/mixin/entity/hostile/VexEntityMixin.java
@@ -0,0 +1,23 @@
+package net.hallowed.oldways.mixin.entity.hostile;
+
+import net.minecraft.entity.mob.EvokerEntity;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.mob.VexEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(VexEntity.class)
+public abstract class VexEntityMixin {
+    @Inject(method = "tick", at = @At("HEAD"))
+    private void oldways$dieWhenEvokerOwnerDies(CallbackInfo ci) {
+        VexEntity self = (VexEntity)(Object)this;
+        if (self.getWorld().isClient()) return;
+
+        MobEntity owner = self.getOwner();
+        if (owner instanceof EvokerEntity && !owner.isAlive()) {
+            self.serverDamage(self.getDamageSources().magic(), Float.MAX_VALUE);
+        }
+    }
+}
