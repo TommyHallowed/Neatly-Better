0fb7fb077658e7b1f79698c806e1621e7dd86b31
TommyHallowed <studenttomas2126@gmail.com>
2025-10-10 14:08:18 +0200

    Removed unused import

--- src/main/java/net/hallowed/oldways/mixin/entity/PlayerEntityMixin.java
+++ src/main/java/net/hallowed/oldways/mixin/entity/PlayerEntityMixin.java
@@ -8,7 +8,6 @@ import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.BowItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.item.RangedWeaponItem;
