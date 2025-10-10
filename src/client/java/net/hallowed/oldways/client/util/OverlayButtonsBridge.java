da63ff1a34be22b365391030b4125d06dae5fdf3
TommyHallowed <studenttomas2126@gmail.com>
2025-10-10 11:29:20 +0200

    Removed comment

--- src/client/java/net/hallowed/oldways/client/util/OverlayButtonsBridge.java
+++ src/client/java/net/hallowed/oldways/client/util/OverlayButtonsBridge.java
@@ -2,7 +2,6 @@ package net.hallowed.oldways.client.util;
 
 import net.minecraft.client.gui.widget.ButtonWidget;
 
-/** Plain helper interface (NOT a mixin). */
 public interface OverlayButtonsBridge {
     ButtonWidget hallowed$getCoordsBtn();
     ButtonWidget hallowed$getTimeBtn();
