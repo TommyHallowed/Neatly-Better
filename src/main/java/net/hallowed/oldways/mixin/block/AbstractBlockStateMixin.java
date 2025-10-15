1b977b78f84e752489f2252a5929fa4c1d600f79
TommyHallowed <studenttomas2126@gmail.com>
2025-10-15 20:32:43 +0200

    Adjusted netherrack, crimson/warped nylium hardness

---
+++ src/main/java/net/hallowed/oldways/mixin/block/AbstractBlockStateMixin.java
@@ -0,0 +1,25 @@
+package net.hallowed.oldways.mixin.block;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockView;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(AbstractBlock.AbstractBlockState.class)
+public abstract class AbstractBlockStateMixin {
+
+    @Shadow public abstract boolean isOf(Block block);
+
+    @Inject(method = "getHardness", at = @At("HEAD"), cancellable = true)
+    private void oldways$setSpecificHardness(BlockView world, BlockPos pos, CallbackInfoReturnable<Float> cir) {
+        if (isOf(Blocks.NETHERRACK) || isOf(Blocks.CRIMSON_NYLIUM) || isOf(Blocks.WARPED_NYLIUM)) {
+            cir.setReturnValue(1.4F);
+        }
+    }
+}
