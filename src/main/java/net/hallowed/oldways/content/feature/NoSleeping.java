67decd48f9ef39edf6eeb610c7fb5fd428e00f0a
TommyHallowed <studenttomas2126@gmail.com>
2025-10-15 02:11:27 +0200

    Moved from bed block mixin

--- src/main/java/net/hallowed/oldways/content/feature/NoSleeping.java
+++ src/main/java/net/hallowed/oldways/content/feature/NoSleeping.java
@@ -9,28 +9,25 @@ import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldProperties;
 
 public final class NoSleeping {
 
     public static void register() {
         UseBlockCallback.EVENT.register((player, world, hand, hit) -> {
             if (!(world instanceof ServerWorld sw)) return ActionResult.PASS;
-
             if (sw.getGameRules().getBoolean(ModGameRules.ALLOW_SLEEP)) return ActionResult.PASS;
 
             BlockPos pos = hit.getBlockPos();
             BlockState state = sw.getBlockState(pos);
-
             if (!(state.getBlock() instanceof BedBlock)) return ActionResult.PASS;
             if (sw.getRegistryKey() != World.OVERWORLD) return ActionResult.PASS;
 
             if (player instanceof ServerPlayerEntity sp) {
-                ServerPlayerEntity.Respawn respawn = new ServerPlayerEntity.Respawn(
-                        sw.getRegistryKey(),
-                        pos,
-                        sp.getYaw(),
-                        true
-                );
+                WorldProperties.SpawnPoint spawnPoint =
+                        WorldProperties.SpawnPoint.create(sw.getRegistryKey(), pos, sp.getYaw(), sp.getPitch());
+
+                ServerPlayerEntity.Respawn respawn = new ServerPlayerEntity.Respawn(spawnPoint, false);
                 sp.setSpawnPoint(respawn, true);
                 sp.swingHand(hand, true);
             }
