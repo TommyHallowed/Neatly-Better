fdcc044ea6155fc2524537a9ac6073e4aed9a41e
TommyHallowed <studenttomas2126@gmail.com>
2025-10-15 02:13:17 +0200

    Removed rocket elytra boosting only for vanilla rockets

--- src/main/java/net/hallowed/oldways/mixin/item/FireworkRocketItemMixin.java
+++ src/main/java/net/hallowed/oldways/mixin/item/FireworkRocketItemMixin.java
@@ -1,9 +1,9 @@
 package net.hallowed.oldways.mixin.item;
 
-import net.hallowed.oldways.init.ModGameRules;
+import net.hallowed.oldways.init.ModItems;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.FireworkRocketItem;
-import net.minecraft.server.world.ServerWorld;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
@@ -16,13 +16,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class FireworkRocketItemMixin {
 
     @Inject(method = "use", at = @At("HEAD"), cancellable = true)
-    private void hallowed$maybeBlockAirUseWhileGliding(World world, PlayerEntity user, Hand hand,
-                                                       CallbackInfoReturnable<ActionResult> cir) {
-        if (user != null && user.isGliding()) {
-            if (!(world instanceof ServerWorld sw)) return;
-            if (sw.getGameRules().getBoolean(ModGameRules.ELYTRA_FIREWORK_BOOSTING)) return;
-            cir.setReturnValue(ActionResult.FAIL);
-            user.stopUsingItem();
-        }
+    private void oldways$onlyDragonBurstBoosts(World world, PlayerEntity user, Hand hand,
+                                               CallbackInfoReturnable<ActionResult> cir) {
+        if (user == null || !user.isGliding()) return;
+        ItemStack stack = user.getStackInHand(hand);
+
+        if (stack.isOf(ModItems.DRAGON_BURST_ROCKET)) return;
+        cir.setReturnValue(ActionResult.PASS);
     }
 }
