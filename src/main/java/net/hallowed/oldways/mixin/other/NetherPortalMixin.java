0ae71a8866b9ee1825686e4e6148cd188c1543c9
TommyHallowed <studenttomas2126@gmail.com>
2025-10-17 02:09:26 +0200

    Added new mixin that allows building nether portals with crying obsidian

---
+++ src/main/java/net/hallowed/oldways/mixin/other/NetherPortalMixin.java
@@ -0,0 +1,26 @@
+package net.hallowed.oldways.mixin.other;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Blocks;
+import net.minecraft.world.dimension.NetherPortal;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(NetherPortal.class)
+public class NetherPortalMixin {
+    @Shadow
+    @Final
+    @Mutable
+    private static AbstractBlock.ContextPredicate IS_VALID_FRAME_BLOCK;
+
+    @Inject(method = "<clinit>", at = @At("RETURN"))
+    private static void onClassInit(CallbackInfo ci) {
+        IS_VALID_FRAME_BLOCK = (state, world, pos) ->
+                state.isOf(Blocks.OBSIDIAN) || state.isOf(Blocks.CRYING_OBSIDIAN);
+    }
+}
