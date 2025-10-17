0981d28e7a210c5859b2d9e19c590029b4a15fe1
TommyHallowed <studenttomas2126@gmail.com>
2025-10-17 02:06:25 +0200

    Removed Render Projectile on other mobs feature (for now)

--- src/client/java/net/hallowed/oldways/client/mixin/entity/EntityRenderManagerMixin.java
+++ src/client/java/net/hallowed/oldways/client/mixin/entity/EntityRenderManagerMixin.java
@@ -1,13 +1,10 @@
 package net.hallowed.oldways.client.mixin.entity;
 
-import net.hallowed.oldways.client.feature.stuckprojectile.StuckProjectilesState;
-import net.hallowed.oldways.client.util.SettingsPrefs;
 import net.minecraft.client.render.command.OrderedRenderCommandQueue;
 import net.minecraft.client.render.entity.EntityRenderManager;
 import net.minecraft.client.render.entity.state.EntityRenderState;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
 import org.joml.Quaternionf;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -21,31 +18,10 @@ import java.util.WeakHashMap;
 
 @Mixin(EntityRenderManager.class)
 public class EntityRenderManagerMixin {
-    @Unique private static final SettingsPrefs OW$prefs = SettingsPrefs.get();
 
-    @Unique @SuppressWarnings("unused")
+    @Unique
     private static final Map<EntityRenderState, Entity> OW$ENTITY_BY_STATE = new WeakHashMap<>();
 
-    @Inject(
-            method = "getAndUpdateRenderState(Lnet/minecraft/entity/Entity;F)Lnet/minecraft/client/render/entity/state/EntityRenderState;",
-            at = @At("HEAD")
-    )
-    private <E extends Entity> void oldways$captureCounts(E e, float tickDelta, CallbackInfoReturnable<EntityRenderState> cir) {
-        if (!OW$prefs.showStuckProjectiles) {
-            StuckProjectilesState.ENTITY_ID = 0;
-            StuckProjectilesState.ARROW_COUNT = 0;
-            StuckProjectilesState.STINGER_COUNT = 0;
-            return;
-        }
-        StuckProjectilesState.ENTITY_ID = e.getId();
-        if (e instanceof LivingEntity living) {
-            StuckProjectilesState.ARROW_COUNT   = living.getStuckArrowCount();
-            StuckProjectilesState.STINGER_COUNT = living.getStingerCount();
-        } else {
-            StuckProjectilesState.ARROW_COUNT = 0;
-            StuckProjectilesState.STINGER_COUNT = 0;
-        }
-    }
 
     @Inject(
             method = "getAndUpdateRenderState(Lnet/minecraft/entity/Entity;F)Lnet/minecraft/client/render/entity/state/EntityRenderState;",
