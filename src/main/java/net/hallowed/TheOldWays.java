5efe6fe392a093e31bbd1262b272bc0ce59a956c
TommyHallowed <studenttomas2126@gmail.com>
2026-03-11 00:28:01 +0100

    Updated

--- src/main/java/net/hallowed/TheOldWays.java
+++ src/main/java/net/hallowed/TheOldWays.java
@@ -1,12 +1,14 @@
 package net.hallowed;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.hallowed.oldways.content.feature.HostileAttributeTweaks;
 import net.hallowed.oldways.content.item.MapBuilderItem;
 import net.hallowed.oldways.api.OWRegistry;
 import net.hallowed.oldways.init.*;
 import net.hallowed.oldways.network.OldWaysNetwork;
+import net.hallowed.oldways.util.FastChunkScanner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,6 +30,7 @@ public class TheOldWays implements ModInitializer {
         ModEvents.init();
         ModDataComponents.init();
         HostileAttributeTweaks.init();
+        ServerLifecycleEvents.SERVER_STARTED.register(FastChunkScanner::restoreAllStructures);
         ServerTickEvents.END_SERVER_TICK.register(server -> MapBuilderItem.MapGenerationQueue.tick());
 
         // 2) Creative tab entries
