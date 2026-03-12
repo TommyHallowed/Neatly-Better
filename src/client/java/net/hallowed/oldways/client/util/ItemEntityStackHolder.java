c8ca5e6eb549065272c011120b8edba3c9d6b6c7
TommyHallowed <studenttomas2126@gmail.com>
2026-03-12 04:45:16 +0100

    Removed

--- src/client/java/net/hallowed/oldways/client/util/ItemEntityStackHolder.java
+++
@@ -1,7 +0,0 @@
-package net.hallowed.oldways.client.util;
-import net.minecraft.world.item.ItemStack;
-
-public interface ItemEntityStackHolder {
-    void oldways$setStack(ItemStack stack);
-    ItemStack oldways$getStack();
-}
