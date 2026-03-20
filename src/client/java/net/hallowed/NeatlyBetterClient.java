79b50e32811540c70f4ae066c2ff06c4d1152626
TommyHallowed <studenttomas2126@gmail.com>
2026-03-20 11:57:34 +0100

    Added Client Config

--- src/client/java/net/hallowed/NeatlyBetterClient.java
+++ src/client/java/net/hallowed/NeatlyBetterClient.java
@@ -1,8 +1,12 @@
 package net.hallowed;
 
+import fuzs.forgeconfigapiport.fabric.api.v5.ConfigRegistry;
+import fuzs.forgeconfigapiport.fabric.api.v5.client.ConfigScreenFactoryRegistry;
+
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.TooltipComponentCallback;
 
+import net.hallowed.neatlybetter.client.config.NTClientConfig;
 import net.hallowed.neatlybetter.client.feature.AutoRefill;
 import net.hallowed.neatlybetter.client.feature.ClientMapPreviewTooltip;
 import net.hallowed.neatlybetter.client.feature.locator.BackpackWaypointsClient;
@@ -13,11 +17,17 @@ import net.hallowed.neatlybetter.client.tooltip.EffectTooltipRenderer;
 import net.hallowed.neatlybetter.client.util.BackpackCheckClient;
 import net.hallowed.neatlybetter.client.util.EnderCheckClient;
 import net.hallowed.neatlybetter.tooltip.MapPreviewTooltip;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+
+import static net.hallowed.NeatlyBetter.MOD_ID;
 
 
 public class NeatlyBetterClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        ConfigRegistry.INSTANCE.register(MOD_ID, ModConfig.Type.CLIENT, NTClientConfig.CONFIG_SPEC);
+        ConfigScreenFactoryRegistry.INSTANCE.register(MOD_ID, ConfigurationScreen::new);
         EnderCheckClient.register();
         EnderWaypointsClient.register();
         BackpackCheckClient.register();
