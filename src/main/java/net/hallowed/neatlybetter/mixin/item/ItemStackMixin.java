a882ce0e9558aef5fec8518df0d4c4e1be2aafaf
TommyHallowed <studenttomas2126@gmail.com>
2026-04-04 19:39:13 +0200

    Moved to AddedItemProperties to match vanilla registration

--- src/main/java/net/hallowed/neatlybetter/mixin/item/ItemStackMixin.java
+++ src/main/java/net/hallowed/neatlybetter/mixin/item/ItemStackMixin.java
@@ -1,7 +1,5 @@
 package net.hallowed.neatlybetter.mixin.item;
 
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
@@ -22,8 +20,6 @@ public abstract class ItemStackMixin {
             if (self.is(Items.GLISTERING_MELON_SLICE)
                     && user.getHealth() < 20.0F) {
                 user.heal(1.0F);
-            } else if (self.is(Items.GLOW_BERRIES)) {
-                user.addEffect(new MobEffectInstance(MobEffects.GLOWING, 200, 0), user);
             }
         }
     }
