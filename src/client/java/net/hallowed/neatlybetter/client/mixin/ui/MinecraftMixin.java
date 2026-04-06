7eadfb5512c436a826ab5bdcd8ef90b706c3c627
TommyHallowed <studenttomas2126@gmail.com>
2026-04-06 05:09:16 +0200

    Music now plays even when player quits to title screen or loads a world from title screen

--- src/client/java/net/hallowed/neatlybetter/client/mixin/ui/MinecraftMixin.java
+++ src/client/java/net/hallowed/neatlybetter/client/mixin/ui/MinecraftMixin.java
@@ -5,7 +5,9 @@ import net.hallowed.neatlybetter.client.config.NTClientConfig;
 import net.hallowed.neatlybetter.client.util.GameRendererPickHelper;
 import net.minecraft.client.Minecraft;
 
+import net.minecraft.client.sounds.SoundManager;
 import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
@@ -17,6 +19,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
@@ -86,5 +89,22 @@ public class MinecraftMixin {
     private void neatlybetter$isTelemetryEnabledByApi(CallbackInfoReturnable<Boolean> cir) {
         if (!NTClientConfig.CONFIG.telemetryOff.get()) return;
         cir.setReturnValue(false);
+    }
+
+    @Unique
+    private static final SoundSource[] SOURCES_TO_STOP = {
+            SoundSource.RECORDS, SoundSource.WEATHER, SoundSource.BLOCKS,
+            SoundSource.HOSTILE, SoundSource.NEUTRAL, SoundSource.PLAYERS,
+            SoundSource.AMBIENT, SoundSource.VOICE, SoundSource.UI
+    };
+
+    @Redirect(
+            method = "updateLevelInEngines(Lnet/minecraft/client/multiplayer/ClientLevel;Z)V",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/sounds/SoundManager;stop()V")
+    )
+    private void NeatlyBetter$RedirectSoundManagerStop(SoundManager instance) {
+        for (SoundSource source : SOURCES_TO_STOP) {
+            instance.stop(null, source);
+        }
     }
 }
