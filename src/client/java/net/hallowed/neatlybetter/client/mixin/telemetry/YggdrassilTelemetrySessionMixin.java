8070318e509601e7e95d88646a64452e77840d4f
TommyHallowed <studenttomas2126@gmail.com>
2026-06-13 20:04:13 +0200

    Fixed telemetry problems

--- src/client/java/net/hallowed/neatlybetter/client/mixin/telemetry/YggdrassilTelemetrySessionMixin.java
+++ src/client/java/net/hallowed/neatlybetter/client/mixin/telemetry/YggdrassilTelemetrySessionMixin.java
@@ -10,17 +10,17 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(YggdrassilTelemetrySession.class)
+@Mixin(value = YggdrassilTelemetrySession.class, remap = false)
 public class YggdrassilTelemetrySessionMixin {
 
-    @Inject(method = "isEnabled", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "isEnabled", at = @At("HEAD"), cancellable = true, remap = false)
     private void neatlybetter$forceDisabled(CallbackInfoReturnable<Boolean> cir) {
         if (NTClientConfig.CONFIG.telemetryOff.get()) {
             cir.setReturnValue(false);
         }
     }
 
-    @Inject(method = "createNewEvent", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "createNewEvent", at = @At("HEAD"), cancellable = true, remap = false)
     private void neatlybetter$returnEmptyEvent(String type, CallbackInfoReturnable<TelemetryEvent> cir) {
         if (NTClientConfig.CONFIG.telemetryOff.get()) {
             cir.setReturnValue(TelemetryEvent.EMPTY);
