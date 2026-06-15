6062d63f030a74cd9b309c50c6718a3decf3648e
TommyHallowed <studenttomas2126@gmail.com>
2026-06-15 16:42:52 +0200

    Remove unused class

--- src/main/java/net/hallowed/neatlybetter/api/EmissiveBannerAccessor.java
+++
@@ -1,8 +0,0 @@
-package net.hallowed.neatlybetter.api;
-
-public interface EmissiveBannerAccessor {
-
-    boolean neatlybetter$isEmissive();
-
-    void neatlybetter$setEmissive(boolean emissive);
-}
