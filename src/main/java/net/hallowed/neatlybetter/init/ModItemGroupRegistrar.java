e3799d527ff89eb1486c0842ab641f507aaf0eda
TommyHallowed <studenttomas2126@gmail.com>
2026-06-15 16:43:30 +0200

    Removed wolf collar from Creative Tab

--- src/main/java/net/hallowed/neatlybetter/init/ModItemGroupRegistrar.java
+++ src/main/java/net/hallowed/neatlybetter/init/ModItemGroupRegistrar.java
@@ -15,7 +15,6 @@ public final class ModItemGroupRegistrar {
         NTRegistry.addToGroup(CreativeModeTabs.TOOLS_AND_UTILITIES, entries -> {
                 entries.insertAfter(Items.MAP , ModItems.MAP_BUILDER);
                 entries.insertAfter(Items.LEAD , ModItems.CHEST_KEY);
-            entries.insertAfter(ModItems.CHEST_KEY, ModItems.WOLF_COLLAR);
         });
 
         NTRegistry.addToGroup(CreativeModeTabs.BUILDING_BLOCKS, entries -> {
