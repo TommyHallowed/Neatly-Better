b12b1aebb2e2bd00e1f4343f0a51c6846f06839b
TommyHallowed <studenttomas2126@gmail.com>
2026-06-16 12:52:25 +0200

    Fixed Click Through sign dyeing

---
+++ src/main/java/net/hallowed/neatlybetter/mixin/entity/player/ServerPlayerGameModeMixin.java
@@ -0,0 +1,33 @@
+package net.hallowed.neatlybetter.mixin.entity.player;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
+
+import net.hallowed.neatlybetter.config.NTCommonConfig;
+
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.level.ServerPlayerGameMode;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.SignApplicator;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(ServerPlayerGameMode.class)
+public class ServerPlayerGameModeMixin {
+
+    @WrapOperation(
+            method = "useItemOn",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerPlayer;isSecondaryUseActive()Z")
+    )
+    private boolean neatlybetter$allowSignApplicatorWhenSneaking(
+            ServerPlayer player,
+            Operation<Boolean> op,
+            @Local(argsOnly = true, name = "itemStack") ItemStack itemStack
+    ) {
+        if (!NTCommonConfig.CONFIG.clickThrough.get()) return false;
+        if (!op.call(player)) return false;
+        return !(itemStack.getItem() instanceof SignApplicator);
+    }
+}
