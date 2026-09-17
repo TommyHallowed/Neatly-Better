18c68ea6d9a3e776852301e354326116aecbe585
TommyHallowed <studenttomas2126@gmail.com>
2026-09-17 22:02:40 +0200

    Fixed crash related to portal shape

--- src/main/java/net/hallowed/neatlybetter/mixin/other/PortalShapeMixin.java
+++ src/main/java/net/hallowed/neatlybetter/mixin/other/PortalShapeMixin.java
@@ -1,7 +1,7 @@
 package net.hallowed.neatlybetter.mixin.other;
 
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.portal.PortalShape;
 
 import org.spongepowered.asm.mixin.Final;
@@ -12,16 +12,17 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.function.Predicate;
+
 @Mixin(PortalShape.class)
 public class PortalShapeMixin {
     @Shadow
     @Final
     @Mutable
-    public static BlockBehaviour.StatePredicate FRAME;
+    public static Predicate<BlockState> FRAME;
 
     @Inject(method = "<clinit>", at = @At("RETURN"))
     private static void onClassInit(CallbackInfo ci) {
-        FRAME = (state, _, _) ->
-                state.is(Blocks.OBSIDIAN) || state.is(Blocks.CRYING_OBSIDIAN);
+        FRAME = (state) -> state.is(Blocks.OBSIDIAN) || state.is(Blocks.CRYING_OBSIDIAN);
     }
 }
